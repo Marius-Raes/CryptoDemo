98bcf59fed90009f996120ba1d012f7e60a35264
Marius Raes <mariusraes@Mariuss-MacBook-Air.local>
2022-08-11 21:37:51 +0200

    basic script done

--- scr/alice.py
+++ scr/alice.py
@@ -5,9 +5,8 @@ import json
 
 keys = RSA.createKeys()
 
-print(keys)
 
-
+print("Alice: Hi bob i am sending you my public RSA key")
 with open('scr/toBob.json', 'w') as fp:
     json.dump(keys['public'], fp)
 
@@ -18,8 +17,12 @@ with open('scr/toAlice.json', 'r') as fp:
     symKeyEncrypted = json.load(fp)
 
 symKey = RSA.decrypt(keys["private"], symKeyEncrypted)
+print("Alice: I decrypted the symetric key using my private key.")
+print("Alice: We both now have the symetric key, i will use it to send an encrypted message to you :)")
 
 encMess = AES.encryptMessage(symKey, "Men hvorfor heter hun da hønse Lovisa?")
 
+with open('scr/toBob.json', 'w') as fp:
+    json.dump({'message': encMess}, fp)
 
-print(AES.decryptMessage(symKeyEncrypted, encMess))
+
--- scr/bob.py
+++ scr/bob.py
@@ -1,3 +1,4 @@
+from time import sleep
 import RSA
 import AES
 import json
@@ -6,7 +7,7 @@ sleep(10)
 with open('scr/toBob.json', 'r') as fp:
     alicePublic = json.load(fp)
 
-print(alicePublic)
+print("Bob: Hi Alice, i am sending you a symetric AES key that i encrypted using the public key you sent me")
 
 symKey = AES.createKey()
 
@@ -16,8 +17,16 @@ with open('scr/toAlice.json', 'w') as fp:
     json.dump(symKeyEncrypted, fp)
 
 
+sleep(10)
+with open('scr/toBob.json', 'r') as fp:
+    encMessFromAlice = json.load(fp)['message']
+
+
+print("Bob: I got your message and decrypted it using the symetric key")
+print("Bob: your message read '{}'".format(AES.decryptMessage(symKey, encMessFromAlice)))
+print("Bob: i am sending you an answer")
 
 encMess = AES.encryptMessage(symKey, "fordi det er Magda baklengs...... Hvis du vrir og snur og vender på bokstavene!")
 
 
-print(AES.decryptMessage(symKey, encMess))
+
